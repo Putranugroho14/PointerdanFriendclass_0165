3fa9c77dd9e6cb050256c55ba41ea217f6433841
Putranugroho14 <putranugroho61@gmail.com>
2024-05-07 10:35:08 +0700

    membuat pointer reference mhs

--- pointer dan friend pemdas/pointer dan friend pemdas.cpp
+++ pointer dan friend pemdas/pointer dan friend pemdas.cpp
@@ -14,5 +14,12 @@ int main()
 
 	mahasiswa mhs{ 1 }; //objek mhs
 	mhs.showNim(); // member access operator
+
+	mahasiswa& refMhs = mhs; //pointer reference refmhs
+	refMhs.nim = 2; //member access operator
+	mhs.showNim();
+
 }
+
+
 
